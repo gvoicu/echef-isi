f03ef23808e5090ceb5ac05c3e41f95014bcfec5
Stefan Neculai <stefan.neculai@gmail.com>
2012-12-15 23:11:07 +0200

    Update vars around.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,112 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20121215201413) do
+
+  create_table "bookings", :force => true do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.boolean  "confirmed"
+    t.integer  "table_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "bookings", ["table_id"], :name => "index_bookings_on_table_id"
+
+  create_table "complaints", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "dish_types", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "dishes", :force => true do |t|
+    t.string   "ingredients"
+    t.integer  "grams"
+    t.float    "price"
+    t.integer  "time"
+    t.integer  "dish_type_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  add_index "dishes", ["dish_type_id"], :name => "index_dishes_on_dish_type_id"
+
+  create_table "notifications", :force => true do |t|
+    t.string   "note"
+    t.integer  "table_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "notifications", ["table_id"], :name => "index_notifications_on_table_id"
+
+  create_table "order_dishes", :force => true do |t|
+    t.integer  "order_id"
+    t.integer  "dish_id"
+    t.integer  "time"
+    t.string   "note"
+    t.integer  "dish_status"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "order_dishes", ["dish_id"], :name => "index_order_dishes_on_dish_id"
+  add_index "order_dishes", ["order_id"], :name => "index_order_dishes_on_order_id"
+
+  create_table "orders", :force => true do |t|
+    t.datetime "closed_at"
+    t.integer  "rating"
+    t.integer  "table_id"
+    t.integer  "waiter_id"
+    t.integer  "chef_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "orders", ["chef_id"], :name => "index_orders_on_chef_id"
+  add_index "orders", ["table_id"], :name => "index_orders_on_table_id"
+  add_index "orders", ["waiter_id"], :name => "index_orders_on_waiter_id"
+
+  create_table "tables", :force => true do |t|
+    t.integer  "number"
+    t.string   "qr_code"
+    t.integer  "persons"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.string   "phone"
+    t.integer  "u_type"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
