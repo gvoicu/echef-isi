36f63362debb9dbadf60b469e9367a311dbea651
Stefan Neculai <stefan.neculai@gmail.com>
2012-12-16 18:53:42 +0200

    Fixes.

--- db/schema.rb
+++ db/schema.rb
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(:version => 20121215201413) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
+    t.string   "username",               :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(:version => 20121215201413) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "email"
     t.string   "name"
     t.string   "phone"
     t.integer  "u_type"
@@ -116,7 +117,7 @@ ActiveRecord::Schema.define(:version => 20121215201413) do
     t.datetime "updated_at",                             :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
