bf0d6bd2d8eb37a98f4c6531637ed9784d94aee2
Stefan Neculai <stefan.neculai@gmail.com>
2012-12-16 19:59:47 +0200

    Add name to dishes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121215201413) do
+ActiveRecord::Schema.define(:version => 20121216175824) do
 
   create_table "bookings", :force => true do |t|
     t.datetime "start"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20121215201413) do
     t.integer  "dish_type_id"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "name"
   end
 
   add_index "dishes", ["dish_type_id"], :name => "index_dishes_on_dish_type_id"
@@ -99,7 +100,7 @@ ActiveRecord::Schema.define(:version => 20121215201413) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
+    t.string   "username",               :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -109,6 +110,7 @@ ActiveRecord::Schema.define(:version => 20121215201413) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "email"
     t.string   "name"
     t.string   "phone"
     t.integer  "u_type"
@@ -116,7 +118,7 @@ ActiveRecord::Schema.define(:version => 20121215201413) do
     t.datetime "updated_at",                             :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
