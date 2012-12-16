3f31b84fddcd148a225ab8ddadeb66b051c8d23c
Stefan Neculai <stefan.neculai@gmail.com>
2012-12-17 00:16:31 +0200

    Create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :u_type, :name, :username
+  validates :username, :uniqueness => true
   # attr_accessible :title, :body
   
   def admin?
