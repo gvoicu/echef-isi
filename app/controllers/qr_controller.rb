45c6dee30f4c90779ce8890bd00b8bf8313bcece
Stefan Neculai <stefan.neculai@gmail.com>
2013-01-08 14:09:33 +0200

    Qr size change.

--- app/controllers/qr_controller.rb
+++ app/controllers/qr_controller.rb
@@ -1,7 +1,7 @@
 class QrController < ApplicationController
   def index
     table = params[:qr_code]
-    @qr = RQRCode::QRCode.new(request.host + '/menu/' + table)
+    @qr = RQRCode::QRCode.new(request.host + '/menu/' + table, :size => 8)
     
     render:layout => false
   end
